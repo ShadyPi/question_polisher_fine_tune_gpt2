b6e41b0cd2deda3aacbc054effad2e1bffae8fa0
ShadyPi <11342288+shadypi@user.noreply.gitee.com>
2023-10-10 14:13:48 +0800

    gpt2-xl

--- try.py
+++ try.py
@@ -1,10 +1,18 @@
 import torch
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-print("GPU 编号: {}".format(device))
-print("GPU 名称: {}".format(torch.cuda.get_device_name(1)))
-
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print("GPU 编号: {}".format(device))
-print("GPU 名称: {}".format(torch.cuda.get_device_name(0)))
+print("GPU id: {}".format(device))
+print("GPU name: {}".format(torch.cuda.get_device_name(0)))
+
+
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+print("GPU id: {}".format(device))
+print("GPU name: {}".format(torch.cuda.get_device_name(1)))
+
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+print("GPU id: {}".format(device))
+print("GPU name: {}".format(torch.cuda.get_device_name(2)))
+
+device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+print("GPU id: {}".format(device))
+print("GPU name: {}".format(torch.cuda.get_device_name(3)))
